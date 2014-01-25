c24f88f86574e430ee3c2e2aab5163cdc89f4114
Mateusz Przybyla <przybyla.mat@gmail.com>
2014-01-25 17:48:42 +0100

    Headers refactoring

--- PairProject/utils.h
+++ PairProject/utils.h
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <vector>
 
+std::vector<double> getRandomValues()
+{
+	std::vector<double> v;
+	for( int i = 0; i < 10; i++ )
+		v.push_back( ( rand() % 100000 ) / 1000. );
+	return v;
+}
+
 void printVector( std::vector<double> v )
 {
 	for( auto it = v.begin(); it != v.end(); it++ )
